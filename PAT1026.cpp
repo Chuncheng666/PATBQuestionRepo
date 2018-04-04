3595b303805bb08207f8a9acf9f6ae579f159c7b
Chuncheng666 <1015830270@qq.com>
2018-04-04 13:43:03 +0800

    PAT1026 -- 程序运行时间
    程序运行时间

---
+++ PAT1026.cpp
@@ -0,0 +1,37 @@
+﻿/*******************************************************************************
+Copyright © 2018-20xx Qiao Chuncheng, All Rights Reserved.
+File name:		PAT1026.cpp
+Author:			Qiao Chuncheng
+Version:		v1.0
+Date:			2018-03-14
+*******************************************************************************/
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t1, t2;
+	int flag, seconds;
+	int formatHour, formatMinute, formatSeconds;
+	while (cin >> t1 >> t2)
+	{
+		flag = (t2 - t1) % 100 / 10;
+		if (flag >= 0 && flag < 5)
+		{
+			seconds = (t2 - t1) / 100;
+		}
+		else
+		{
+			seconds = (t2 - t1) / 100 + 1;
+		}
+		formatHour = seconds / 3600;
+		formatMinute = seconds % 3600 / 60;
+		formatSeconds = seconds % 3600 % 60;
+		printf("%02d:%02d:%02d\n", formatHour, formatMinute, formatSeconds);
+	}
+    return 0;
+}
+
