8aec627cd0031b1a389284c0d5cec6a6737dcb21
Chuncheng666 <1015830270@qq.com>
2018-04-04 15:53:19 +0800

    fix bug in 1001.cpp, 没有考虑n == 1的情况

--- PAT1001.cpp
+++ PAT1001.cpp
@@ -1,40 +1,15 @@
-// PAT1001.cpp : Defines the entry point for the console application.
-//
+/*******************************************************************************
+Copyright © 2018-20xx Qiao Chuncheng, All Rights Reserved.
+File name:		PAT1001.cpp
+Author:			Qiao Chuncheng		
+Version:		v1.0
+Date:           2018-04-04
+*******************************************************************************/
 
 #include "stdafx.h"
 #include <iostream>
 
 using namespace std;
-
-int GetTimes(int x, int &count)
-{
-	int temp = 0;
-	if (x % 4 == 0)
-	{
-		temp = x;
-		while (temp != 1)
-		{
-			temp = temp / 2;
-			count++;
-		}
-		return 0;
-	}
-	else if (x % 2 == 0)
-	{
-		temp = x / 2;
-		count++;
-		temp = 3 * temp + 1;
-		GetTimes(temp, count);
-	}
-	else
-	{
-		temp = 3 * x + 1;
-		temp = temp / 2;
-		count++;
-		GetTimes(temp, count);
-	}
-	return 1;
-}
 
 int main()
 {
@@ -42,7 +17,13 @@ int main()
 	while (cin >> n)
 	{
 		int count = 0;
-		GetTimes(n, count);
+		while (n != 1)
+		{
+			if (n % 2 != 0)
+				n = 3 * n + 1;
+			n = n / 2;
+			count++;
+		}
 		cout << count << endl;
 	}
     return 0;
